cd95f1e11652c39da5036fb77b8747b7be760c57
94cstyles <cstyles@qq.com>
2022-02-21 17:38:14 +0800

    feat: 添加 typeorm 示例

--- src/server/entity/Member.ts
+++ src/server/entity/Member.ts
@@ -1,6 +1,20 @@
 import { EntityModel } from '@midwayjs/orm';
-import Basic from './Basic.abstract';
-
+import { Column, PrimaryColumn, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn } from 'typeorm';
 
 @EntityModel()
-export default class Member extends Basic {}
+export default class Member {
+    @PrimaryColumn({ unique: true, length: 20 })
+    id: string;
+
+    @Column({ length: 50 })
+    ad: string;
+   
+    @Column({ type: 'boolean', default: false, select: false })
+    isDeleted: boolean;
+    
+    @CreateDateColumn({ select: false })
+    ctime: Date;
+
+    @UpdateDateColumn({ select: false })
+    mtime: Date;
+}
